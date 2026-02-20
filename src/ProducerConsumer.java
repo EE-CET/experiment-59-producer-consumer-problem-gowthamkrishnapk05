9de171feef9566312462c54d35cd0a621429a07d
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-02-20 09:02:39 +0530

    updated

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -1,42 +1,73 @@
-class SharedResource {
-    int item;
-    boolean available = false;
+class SharedBuffer {
+    private int item;
+    private boolean available = false;
 
-    // TODO: synchronize void put(int item)
-    // while(available) -> wait()
-    // set this.item = item, available = true
-    // print "Produced: " + item
-    // notify()
+    public synchronized void produce(int value) throws InterruptedException {
+        while (available) {
+            wait();
+        }
 
-    // TODO: synchronize void get()
-    // while(!available) -> wait()
-    // print "Consumed: " + item
-    // available = false
-    // notify()
+        item = value;
+        System.out.println("Produced: " + item);
+
+        available = true;
+        notifyAll();   // safer than notify
+    }
+
+    public synchronized void consume() throws InterruptedException {
+        while (!available) {
+            wait();
+        }
+
+        System.out.println("Consumed: " + item);
+
+        available = false;
+        notifyAll();   // safer than notify
+    }
 }
 
 class Producer extends Thread {
-    SharedResource resource;
-    // TODO: Constructor to init resource
-    
-    // TODO: run()
-    // Loop 1 to 5
-    // call resource.put(i)
+    private SharedBuffer buffer;
+
+    Producer(SharedBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        try {
+            for (int i = 1; i <= 5; i++) {
+                buffer.produce(i);
+                Thread.sleep(100); // helps console ordering
+            }
+        } catch (InterruptedException e) {}
+    }
 }
 
 class Consumer extends Thread {
-    SharedResource resource;
-    // TODO: Constructor to init resource
-    
-    // TODO: run()
-    // Loop 1 to 5
-    // call resource.get()
+    private SharedBuffer buffer;
+
+    Consumer(SharedBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        try {
+            for (int i = 1; i <= 5; i++) {
+                buffer.consume();
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {}
+    }
 }
 
 public class ProducerConsumer {
     public static void main(String[] args) {
-        // TODO: Create SharedResource object
-        // TODO: Create Producer and Consumer threads
-        // TODO: Start both threads
+        SharedBuffer buffer = new SharedBuffer();
+
+        Consumer c = new Consumer(buffer);
+        Producer p = new Producer(buffer);
+
+        c.start();   // start consumer first
+        p.start();
     }
 }
